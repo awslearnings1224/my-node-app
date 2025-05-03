c5936299c448652e55f73dad455851cfd15d4573
pushkardevops <awslearnings1224@gmail.com>
2025-05-03 09:33:19 +0530

    code build change

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+#cd /home/ec2-user/my-node-app
+#sudo npm install
+#sudo npm start
+#sudo npm start > app.log 2>&1 &
+#!/bin/bash
 cd /home/ec2-user/my-node-app
-sudo npm install
-sudo npm start
-# sudo npm start > app.log 2>&1 &
+
+# Install dependencies
+npm install
+
+# Start the app in background
+nohup node app/server.js > server.log 2>&1 &
+
