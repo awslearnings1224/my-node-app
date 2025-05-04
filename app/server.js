445e4a95ddf01b34da19e218ac2bd0f87549228f
pushkardevops <awslearnings1224@gmail.com>
2025-05-04 08:37:37 +0530

    code build change

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('New from Node.js>>>>>>>>>>>>>>>>>>>>>>>>>>>>!');
+  res.send('New from Node.js****************************!');
 });
 
 app.listen(port, () => {
